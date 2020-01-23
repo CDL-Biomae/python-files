82bf1f3a2ae82c4c34d31db05bf33af874841b0d
Tazzz <thomas.pennamen@gmail.com>
2020-01-23 17:24:59 +0100

    Deleted test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from query import Query
 
-New_Query = Query(column=["city",], table="agency", distinct=True)
+New_Query = Query(script="CREATE TABLE r2_constant (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), nature VARCHAR(255), value VARCHAR(255))")
 
-print(New_Query)
+print(New_Query.execute())
