6253ce30d606d58d50854fb8da6ab1ce0175fc57
Tazzz <thomas.pennamen@gmail.com>
2020-01-24 11:19:50 +0100

    Deleted

--- test.py
+++
@@ -1,5 +0,0 @@
-from query import Query
-
-New_Query = Query(script="CREATE TABLE r2_constant (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), nature VARCHAR(255), value VARCHAR(255))")
-
-print(New_Query.execute())
