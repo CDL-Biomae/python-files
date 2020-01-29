ef1e006557b9fb9db77bcb40ee954424bb9aacaf
Thomas Furgé <th.furge@gmail.com>
2020-01-29 17:16:52 +0100

    Algo dates clées terminé, il reste des tests et du debug a faire

--- dateFiller.py
+++ dateFiller.py
@@ -4,97 +4,257 @@ campaigns = QueryScript(script='SELECT id, reference FROM campaign').execute()
 # print(campaigns)
 
 #%%
+
+def datesCles(id_mp):
+    exposureconditions = QueryScript(
+        script='SELECT step, recordedAt, barrel FROM measureexposurecondition WHERE measurepoint_id=' + str(
+            id_mp)).execute()
+    steps_barrels = [(x[0], x[2]) for x in exposureconditions]
+    print(steps_barrels)
+    print(exposureconditions[0])
+
+    # Dictionnaire de dates cles à remplir
+    dates_cles = {}
+
+    # (step 50, R0) Transplantation Alimentation
+    try:
+        idx = steps_barrels.index((50, 'R0'))
+        temp_date = exposureconditions[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Transplantation Alimentation'] = {'date': temp_date, 'step': 50, 'barrel': 'R0'}
+
+    # (step 60, R7) Recuperation Alimentation
+    try:
+        idx = steps_barrels.index((60, 'R7'))
+        temp_date = exposureconditions[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Recuperation Alimentation'] = {'date': temp_date, 'step': 60, 'barrel': 'R7'}
+
+    # (step 20) Lancement Alimentation
+    try:
+        idx = steps_barrels.index((20, None))
+        temp_date = exposureconditions[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Lancement Alimentation'] = {'date': temp_date, 'step': 20, 'barrel': None}
+
+    # (step 140, RN) Recuperation Reprotoxicité
+    try:
+        idx = steps_barrels.index((140, 'RN'))
+        temp_date = exposureconditions[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Recuperation Reprotoxicite'] = {'date': temp_date, 'step': 140, 'barrel': 'RN'}
+
+    # (step 170) Arrêt Reprotoxicité
+    try:
+        idx = steps_barrels.index((170, None))
+        temp_date = exposureconditions[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Arret Reprotoxicite'] = {'date': temp_date, 'step': 170, 'barrel': None}
+
+    # (step 50, C0) Lancement Chimie (=Lancement reprotoxicité)
+    try:
+        idx = steps_barrels.index((50, 'C0'))
+        temp_date = exposureconditions[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Lancement Chimie'] = {'date': temp_date, 'step': 50, 'barrel': 'C0'}
+
+    # (step 100, R21) Récupération Chimie
+    try:
+        idx = steps_barrels.index((100, 'R21'))
+        temp_date = exposureconditions[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Recuperation Chimie'] = {'date': temp_date, 'step': 100, 'barrel': 'R21'}
+
+    return dates_cles
+
+def datesClesFusion(id_mp_alim, id_mp_chimie, id_mp_repro):
+    exposureconditions_alim = QueryScript(
+        script='SELECT step, recordedAt, barrel FROM measureexposurecondition WHERE measurepoint_id=' + str(
+            id_mp_alim)).execute()
+    exposureconditions_chimie = QueryScript(
+        script='SELECT step, recordedAt, barrel FROM measureexposurecondition WHERE measurepoint_id=' + str(
+            id_mp_chimie)).execute()
+    exposureconditions_repro = QueryScript(
+        script='SELECT step, recordedAt, barrel FROM measureexposurecondition WHERE measurepoint_id=' + str(
+            id_mp_repro)).execute()
+
+    steps_barrels_alim = [(x[0], x[2]) for x in exposureconditions_alim]
+    steps_barrels_chimie = [(x[0], x[2]) for x in exposureconditions_chimie]
+    steps_barrels_repro = [(x[0], x[2]) for x in exposureconditions_repro]
+
+    print(steps_barrels_alim)
+    print(steps_barrels_chimie)
+    print(steps_barrels_repro)
+
+    # Dictionnaire de dates cles à remplir
+    dates_cles = {}
+
+    # (step 50, R0) Transplantation Alimentation
+    try:
+        idx = steps_barrels_alim.index((50, 'R0'))
+        temp_date = exposureconditions_alim[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Transplantation Alimentation'] = {'date': temp_date, 'step': 50, 'barrel': 'R0'}
+
+    # (step 60, R7) Recuperation Alimentation
+    try:
+        idx = steps_barrels_alim.index((60, 'R7'))
+        temp_date = exposureconditions_alim[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Recuperation Alimentation'] = {'date': temp_date, 'step': 60, 'barrel': 'R7'}
+
+    # (step 20) Lancement Alimentation
+    try:
+        idx = steps_barrels_alim.index((20, None))
+        temp_date = exposureconditions_alim[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Lancement Alimentation'] = {'date': temp_date, 'step': 20, 'barrel': None}
+
+    # (step 140, RN) Recuperation Reprotoxicité
+    try:
+        idx = steps_barrels_repro.index((140, 'RN'))
+        temp_date = exposureconditions_repro[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Recuperation Reprotoxicite'] = {'date': temp_date, 'step': 140, 'barrel': 'RN'}
+
+    # (step 170) Arrêt Reprotoxicité
+    try:
+        idx = steps_barrels_repro.index((170, None))
+        temp_date = exposureconditions_repro[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Arret Reprotoxicite'] = {'date': temp_date, 'step': 170, 'barrel': None}
+
+    # (step 50, C0) Lancement Chimie (=Lancement reprotoxicité)
+    try:
+        idx = steps_barrels_chimie.index((50, 'C0'))
+        temp_date = exposureconditions_chimie[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Lancement Chimie'] = {'date': temp_date, 'step': 50, 'barrel': 'C0'}
+
+    # (step 100, R21) Récupération Chimie
+    try:
+        idx = steps_barrels_chimie.index((100, 'R21'))
+        temp_date = exposureconditions_chimie[idx][1]
+    except ValueError:
+        temp_date = None
+
+    dates_cles['Recuperation Chimie'] = {'date': temp_date, 'step': 100, 'barrel': 'R21'}
+
+    return dates_cles
+
+def intersection(liste1, liste2):
+    liste3 = [x for x in liste1 if x in liste2]
+    return liste3
+
+def independance(id_mp1, id_mp2):
+    natures_mp1 = QueryScript(script='SELECT nature FROM pack WHERE measurepoint_id = ' + str(id_mp1)).execute()
+    natures_mp2 = QueryScript(script='SELECT nature FROM pack WHERE measurepoint_id = ' + str(id_mp2)).execute()
+
+    inter_natures = intersection(natures_mp1, natures_mp2)
+    natures = {}
+
+    if len(inter_natures) != 0:
+        return True, natures
+
+    else:
+        for nature in natures_mp1:
+            natures[nature] = id_mp1
+        for nature in natures_mp2:
+            natures[nature] = id_mp2
+
+        return False, natures
+
+print(independance(2895, 2944))
+print(datesClesFusion(2944, 2895, 2895))
+#%%
+
 id_campaigns = [x[0] for x in campaigns]
 cap = False
 
 for id_c in id_campaigns:
+    break
     places = QueryScript(script='SELECT id, reference, type FROM place WHERE campaign_id='+str(id_c)).execute()
     n_places = len(places)
     for i in range(n_places):
         id_p, ref_p, type_p = places[i]
+        measurepoints = QueryScript(script='SELECT id, reference FROM measurepoint WHERE place_id=' + str(id_p)).execute()
+        id_measurepoints = [x[0] for x in measurepoints]
 
         if type_p == 'work_monitoring':
-            measurepoints = QueryScript(script='SELECT id, reference FROM measurepoint WHERE place_id='+str(id_p)).execute()
-            # print(measurepoints)
-            id_measurepoints = [x[0] for x in measurepoints]
-
             for id_mp in id_measurepoints:
-                exposureconditions = QueryScript(script='SELECT step, recordedAt, barrel FROM measureexposurecondition WHERE measurepoint_id='+str(id_mp)).execute()
-                steps_barrels = [(x[0], x[2]) for x in exposureconditions]
-                print(steps_barrels)
-                print(exposureconditions[1])
+                dates_cles = datesCles(id_mp)
 
-                # Dictionnaire de dates cles à remplir
-                dates_cles = {}
+        if type_p == 'point_monitoring':
 
-                # (step 50, R0) Transplantation Alimentation
-                try:
-                    idx = steps_barrels.index((50, 'R0'))
-                    temp_date = exposureconditions[idx][1]
-                except ValueError:
-                    temp_date = None
+            # S'il n'y a qu'un seul point de mesure à un endroit (place) il n'y a aucune fusion de dates a faire
+            if len(id_measurepoints) == 1:
+                id_mp = id_measurepoints[0]
+                exposureconditions = QueryScript(script='SELECT step, recordedAt, barrel FROM measureexposurecondition WHERE measurepoint_id=' + str(id_mp)).execute()
+                # dates_cles = datesCles(exposureconditions)
 
-                dates_cles['Transplantation Alimentation'] = {'date': temp_date, 'step': 50, 'barrel': 'R0'}
+            # Pas encore fait..
+            # S'il y a plus de 3 points de mesure à un endroit (place) --> gérer comme si les points étaient indépendants
+            elif len(id_measurepoints) > 2:
+                # print('\n[+] id_place = ', id_p)
+                # print('[+] point_monitoring --> id_measurepoints = ', id_measurepoints)
+                for id_mp in id_measurepoints:
+                    natures = QueryScript(script='SELECT nature FROM pack WHERE measurepoint_id=' + str(id_mp)).execute()
 
-                # (step 60, R7) Recuperation Alimentation
-                try:
-                    idx = steps_barrels.index((60, 'R7'))
-                    temp_date = exposureconditions[idx][1]
-                except ValueError:
-                    temp_date = None
+                    # print(steps_barrels)
+                    # print(exposureconditions[0])
 
-                dates_cles['Recuperation Alimentation'] = {'date': temp_date, 'step': 60, 'barrel': 'R7'}
+            # Dernier cas: S'il y a exactement 2 points de mesure à un endroit <=> possibles biotests effectués à des périodes différentes donc fusion des dates
+            # sinon voir les points comme indépendants
+            else:
+                print('\n[+] id_place = ', id_p)
+                # print('[+] point_monitoring --> id_measurepoints = ', id_measurepoints)
 
-                # (step 20) Lancement Alimentation
-                try:
-                    idx = steps_barrels.index((20, None))
-                    temp_date = exposureconditions[idx][1]
-                except ValueError:
-                    temp_date = None
+                [id_mp1, id_mp2] = id_measurepoints
+                resultat, natures = independance(id_mp1, id_mp2)
+                print(natures)
 
-                dates_cles['Lancement Alimentation'] = {'date': temp_date, 'step': 20, 'barrel': None}
+                if resultat:  # Si les points sont indépendants (c'est à dire qu'ils ont des types de biotests communs)
+                    for id_mp in id_measurepoints:
+                        dates_cles = datesCles(id_mp)
 
-                # (step 140, RN) Recuperation Reprotoxicité
-                try:
-                    idx = steps_barrels.index((140, 'RN'))
-                    temp_date = exposureconditions[idx][1]
-                except ValueError:
-                    temp_date = None
+                else:
+                    id_mp_alim = natures['alimentation']
+                    id_mp_chimie = natures['chemistry']
+                    id_mp_repro = natures['reproduction']
 
-                dates_cles['Recuperation Reprotoxicite'] = {'date': temp_date, 'step': 140, 'barrel': 'RN'}
+                    if id_mp_repro != id_mp_chimie:
+                        print('\n /!\\ Erreur le point de mesure pour la chimie est différent du point de mesure pour la reproduction')
+                    dates_cles = datesClesFusion(id_mp_alim, id_mp_chimie, id_mp_repro)
 
-                # (step 170) Arrêt Reprotoxicité
-                try:
-                    idx = steps_barrels.index((170, None))
-                    temp_date = exposureconditions[idx][1]
-                except ValueError:
-                    temp_date = None
 
-                dates_cles['Arret Reprotoxicite'] = {'date': temp_date, 'step': 170, 'barrel': None}
-
-                # (step 50, C0) Lancement Chimie (=Lancement reprotoxicité)
-                try:
-                    idx = steps_barrels.index((50, 'C0'))
-                    temp_date = exposureconditions[idx][1]
-                except ValueError:
-                    temp_date = None
-
-                dates_cles['Lancement Alimentation'] = {'date': temp_date, 'step': 50, 'barrel': 'C0'}
-
-                # (step 100, R21) Récupération Chimie
-                try:
-                    idx = steps_barrels.index((100, 'R21'))
-                    temp_date = exposureconditions[idx][1]
-                except ValueError:
-                    temp_date = None
-
-                dates_cles['Lancement Alimentation'] = {'date': temp_date, 'step': 100, 'barrel': 'R21'}
-
-                print(dates_cles)
-
-                cap = True
-                break
-
+            # cap = True
+            # break
     if cap:
         break
 
