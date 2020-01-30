001c3c65e6e1fc9884a3a08ee9e810328c4bc77d
Tazzz <thomas.pennamen@gmail.com>
2020-01-30 15:51:11 +0100

    Correct mistake

--- test1.py
+++
@@ -1,9 +0,0 @@
-# import reference_filler
-
-# reference_filler.run()
-
-from query import QueryScript
-
-output = QueryScript("SELECT specimen_size_px FROM measurereprotoxicity WHERE specimen_size_px IS NOT NULL AND pack_id=6393").execute()
-for i in output :
-    print(i)
