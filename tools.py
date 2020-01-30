05e2b9df9a32b11a7be05c1537d79bc5a8ced1cb
Julien Bronner <julien.bronner@ecl17.ec-lyon.fr>
2020-01-30 17:14:49 +0100

    rajout fonction list_to_dict

--- tools.py
+++ tools.py
@@ -1,5 +1,15 @@
 from query import QueryScript
 
-def pack_finder(measurepoint_id) :
+
+def pack_finder(measurepoint_id):
     return QueryScript("SELECT id FROM pack WHERE measurepoint_id="+str(measurepoint_id)).execute()
-    
+
+
+def list_to_dict(liste):  # liste avec en premier l'element qui servira de clef
+    dico = dict()
+    for elt in liste:
+        if len(elt) == 2:
+            dico[elt[0]] = elt[1]
+        else:
+            dico[elt[0]] = elt[1:]
+    return dico
