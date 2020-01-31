9ea43aa4e5a847f27a523411b96b64780035e28a
Tazzz <thomas.pennamen@gmail.com>
2020-01-31 09:41:22 +0100

    Regroup alimentation function

--- calcul/toxicity/__init__.py
+++ calcul/toxicity/__init__.py
@@ -1,4 +1,2 @@
-from .alimentation_inhibition import alimentation_inhibition
-from .leaf_size import leaf_size
+from .alimentation import alimentation
 from .neurotoxicity import neurotoxicity
-from .specimen_size import specimen_size
