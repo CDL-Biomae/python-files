f8df69548ba7cac3ca0cf596c2a8ce5786b75ddd
Tazzz <thomas.pennamen@gmail.com>
2020-02-04 17:27:32 +0100

    Added report chemistry

--- calcul/chemistry/__init__.py
+++ calcul/chemistry/__init__.py
@@ -1,1 +1,3 @@
-from .nqe import * 
+from .nqe import elements_fish, elements_crustacean, maximum_freq_quanti
+from .result_by_pack_and_sandre import result_by_pack_and_sandre
+from .report import report_data
