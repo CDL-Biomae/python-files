f86f035a43557bdaed514dd92bc7d6cc7267e561
Tazzz <thomas.pennamen@gmail.com>
2020-02-06 11:32:53 +0100

    Change dict to dictonnary

--- tools/list_to_dict.py
+++ tools/list_to_dict.py
@@ -1,8 +1,8 @@
 def list_to_dict(liste):  # liste avec en premier l'element qui servira de clef
-    dict = dict()
+    dictionnary = dict()
     for elt in liste:
         if len(elt) == 2:
-            dict[elt[0]] = elt[1]
+            dictionnary[elt[0]] = elt[1]
         else:
-            dict[elt[0]] = elt[1:]
-    return dict
+            dictionnary[elt[0]] = elt[1:]
+    return dictionnary
