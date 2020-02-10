03a034118bdfe5c1ff241e9c829ffb24593d1d79
Julien Bronner <julien.bronner@ecl17.ec-lyon.fr>
2020-02-10 13:09:52 +0100

    Début creation fichier word rapport experimentation

--- report/__init__.py
+++ report/__init__.py
@@ -3,6 +3,7 @@ from .initialization import *
 from .xl_stations import *
 from .xl_physicochimie import *
 from .word_rapport_experimentation_recuperation_donnees import *
+from .word_rapport_experimentation_creation_fichier import *
 
 # /!\ A METTRE EN DERNIER /!\ #
 from .main import main
