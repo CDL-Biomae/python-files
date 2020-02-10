8a0d830e0ee567147cb654a6690e357af4fc515c
Tazzz <thomas.pennamen@gmail.com>
2020-02-10 14:38:24 +0100

    Added word rapport

--- report/__init__.py
+++ report/__init__.py
@@ -4,6 +4,8 @@ from .initialization import *
 from .xl_stations import *
 from .xl_physicochimie import *
 from .xl_tox import *
+from .word_rapport_experimentation_creation_fichier import *
+from .word_rapport_experimentation_recuperation_donnees import *
 
 # /!\ A METTRE EN DERNIER /!\ #
 from .main import main
