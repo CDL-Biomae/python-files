d269cb175d31daabf30f61764df2dbbc0e03c034
Tazzz <thomas.pennamen@gmail.com>
2020-02-11 14:50:27 +0100

    Added xl_chemistry

--- report/__init__.py
+++ report/__init__.py
@@ -1,5 +1,6 @@
 from .measurepoint_result import measurepoint_result
 from .xl_campagnes import *
+from .xl_chemistry import *
 from .initialization import *
 from .xl_stations import *
 from .xl_physicochimie import *
