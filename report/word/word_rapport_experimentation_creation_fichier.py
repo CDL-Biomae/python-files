5f56d039082eabcce6ec3c8a90d5600c6bba7427
Julien Bronner <julien.bronner@ecl17.ec-lyon.fr>
2020-02-13 17:21:31 +0100

    Changement reference word experimentation

--- report/word/word_rapport_experimentation_creation_fichier.py
+++ report/word/word_rapport_experimentation_creation_fichier.py
@@ -10,11 +10,10 @@ def create_doc(campaign):
     font.name = "Arial"
     dico_exposure_condition, dico_avg_tempe, dico_geo_mp, dico_geo_agency, dico_type_biotest = recuperation_donnee(
         campaign)
-    nb_measurepoint = len(dico_avg_tempe)
-    for i in range(1, nb_measurepoint+1):
+    liste_reference = list(dico_avg_tempe.keys())
+    for reference in liste_reference:
         doc.add_page_break()
         table_geo = doc.add_table(rows=8, cols=4)
-
         for j in range(2, 8):
             table_geo.cell(j, 0).merge(table_geo.cell(j, 1))
         for j in range(2, 5):
@@ -22,54 +21,55 @@ def create_doc(campaign):
 
         header = table_geo.rows[0].cells
         header[0].merge(header[-1])
-        case_header = table_geo.cell(0, 0).paragraphs[0].add_run(dico_geo_agency[i]['code'] +
-                                                                 " : " + dico_geo_agency[i]['name'])
+        case_header = table_geo.cell(0, 0).paragraphs[0].add_run(dico_geo_agency[reference]['code'] +
+                                                                 " : " + dico_geo_agency[reference]['name'])
         case_header.bold = True
         case_header = table_geo.cell(0, 0).paragraphs[0].alignment = 1
 
         table_geo.cell(1, 0).paragraphs[0].add_run('Commune :').bold = True
         table_geo.cell(1, 1).paragraphs[0].add_run(
-            dico_geo_agency[i]['city'] + "    " + dico_geo_agency[i]['zipcode'])
+            dico_geo_agency[reference]['city'] + "    " + dico_geo_agency[reference]['zipcode'])
         table_geo.cell(1, 2).paragraphs[0].add_run(
             "Cours d'eau : ").bold = True
         table_geo.cell(1, 3).paragraphs[0].add_run(
-            dico_geo_agency[i]['stream'])
+            dico_geo_agency[reference]['stream'])
 
         table_geo.cell(2, 0).paragraphs[0].add_run("Biotests :").bold = True
-        biotest_francais = traduction_type_biotest(dico_type_biotest[i])
+        biotest_francais = traduction_type_biotest(
+            dico_type_biotest[reference])
         table_geo.cell(2, 2).paragraphs[0].add_run(
             biotest_francais)
 
         table_geo.cell(3, 0).paragraphs[0].add_run(
             "Réseau de surveillance :").bold = True
         table_geo.cell(3, 2).paragraphs[0].add_run(
-            dico_geo_agency[i]['network'])
+            dico_geo_agency[reference]['network'])
 
         table_geo.cell(4, 0).paragraphs[0].add_run(
             "Type d'hydroécorégion :").bold = True
         table_geo.cell(4, 2).paragraphs[0].add_run(
-            dico_geo_agency[i]['hydroecoregion'])
+            dico_geo_agency[reference]['hydroecoregion'])
 
         table_geo.cell(5, 0).paragraphs[0].add_run(
             "Coordonnées Agence Lambert 93 :").bold = True
         table_geo.cell(5, 2).paragraphs[0].add_run('Y ' +
-                                                   dico_geo_agency[i]['lambertY'])
+                                                   dico_geo_agency[reference]['lambertY'])
         table_geo.cell(5, 3).paragraphs[0].add_run('X ' +
-                                                   dico_geo_agency[i]['lambertX'])
+                                                   dico_geo_agency[reference]['lambertX'])
 
         table_geo.cell(6, 0).paragraphs[0].add_run(
             "Coordonnées BIOMÆ en degrés décimaux : ").bold = True
         table_geo.cell(6, 2).paragraphs[0].add_run(
-            str(dico_geo_mp[i]['longitudeSpotted']))
+            str(dico_geo_mp[reference]['longitudeSpotted']))
         table_geo.cell(6, 3).paragraphs[0].add_run(
-            str(dico_geo_mp[i]['latitudeSpotted']))
+            str(dico_geo_mp[reference]['latitudeSpotted']))
 
         table_geo.cell(7, 0).paragraphs[0].add_run(
             "Coordonnées BIOMÆ Lambert 93 : ").bold = True
         table_geo.cell(7, 2).paragraphs[0].add_run('Y ' +
-                                                   dico_geo_mp[i]['lambertYSpotted'])
+                                                   dico_geo_mp[reference]['lambertYSpotted'])
         table_geo.cell(7, 3).paragraphs[0].add_run('X ' +
-                                                   dico_geo_mp[i]['lambertXSpotted'])
+                                                   dico_geo_mp[reference]['lambertXSpotted'])
 
         doc.add_page_break()
 
@@ -77,12 +77,12 @@ def create_doc(campaign):
         table_image.cell(0, 0).merge(table_image.cell(0, 1))
         table_image.cell(1, 0).merge(table_image.cell(1, 1))
 
-        table_image.cell(0, 0).paragraphs[0].add_run(dico_geo_agency[i]['code'] +
-                                                     " : " + dico_geo_agency[i]['name']).bold = True
+        table_image.cell(0, 0).paragraphs[0].add_run(dico_geo_agency[reference]['code'] +
+                                                     " : " + dico_geo_agency[reference]['name']).bold = True
         table_image.cell(0, 0).paragraphs[0].alignment = 1
 
         table_image.cell(1, 0).paragraphs[0].add_run(
-            "Photos de la station de mesure de la qualité des eaux pour la campagne " + campaign[-2:] + "-" + dico_exposure_condition[i]["J+0"]["date"][6:10]).bold = True  # Mettre que l'année, passage en argument ou autre méthode de récupération ?
+            "Photos de la station de mesure de la qualité des eaux pour la campagne " + campaign[-2:] + "-" + dico_exposure_condition[reference]["J+0"]["date"][6:10]).bold = True  # Mettre que l'année, passage en argument ou autre méthode de récupération ?
         table_image.cell(1, 0).paragraphs[0].alignment = 1
 
         table_image.cell(3, 0).text = "Aval de zone d’encagement"
@@ -116,8 +116,8 @@ def create_doc(campaign):
             "Type de système d’exposition : ").bold = True
 
         # Vérifier avec Biomae que juste ça suffit
-        type_barrel_J0 = dico_exposure_condition[i]['J+0']['type']
-        # type_barrel_J14 = dico_exposure_condition[i]['J+14']['type']
+        type_barrel_J0 = dico_exposure_condition[reference]['J+0']['type']
+        # type_barrel_J14 = dico_exposure_condition[reference]['J+14']['type']
         # type_barrel_J0 = type_barrel_J14 if type_barrel_J14 else type_barrel_J0
         # type_barrel_J14 = type_barrel_J0 if type_barrel_J0 else type_barrel_J14
         # if (type_barrel_J0 == 'barrel') & (type_barrel_J14 == 'barrel'):
@@ -131,7 +131,7 @@ def create_doc(campaign):
         table_image.cell(6, 1).paragraphs[0].add_run(type_barrel_J0)
         table_image.cell(7, 0).merge(table_image.cell(7, 1))
         table_image.cell(7, 0).paragraphs[0].add_run(
-            "Paramètres physico-chimiques pour la campagne : " + campaign[-2:] + "-" + dico_exposure_condition[i]["J+0"]["date"][6:10]).bold = True
+            "Paramètres physico-chimiques pour la campagne : " + campaign[-2:] + "-" + dico_exposure_condition[reference]["J+0"]["date"][6:10]).bold = True
 
         table_temperature = doc.add_table(rows=2, cols=4, style="Table Grid")
         table_temperature.cell(0, 0).merge(table_temperature.cell(1, 0))
@@ -149,13 +149,13 @@ def create_doc(campaign):
             "Maximum")  # .bold = True
         table_temperature.cell(0, 3).paragraphs[0].alignment = 1
         table_temperature.cell(1, 1).paragraphs[0].add_run(str(round(
-            dico_avg_tempe[i]['min'], 1)))
+            dico_avg_tempe[reference]['min'], 1)))
         table_temperature.cell(1, 1).paragraphs[0].alignment = 1
         table_temperature.cell(1, 2).paragraphs[0].add_run(str(round(
-            dico_avg_tempe[i]['average'], 1)))
+            dico_avg_tempe[reference]['average'], 1)))
         table_temperature.cell(1, 2).paragraphs[0].alignment = 1
         table_temperature.cell(1, 3).paragraphs[0].add_run(str(round(
-            dico_avg_tempe[i]['max'], 1)))
+            dico_avg_tempe[reference]['max'], 1)))
         table_temperature.cell(1, 3).paragraphs[0].alignment = 1
         for row in range(2):
             for col in range(4):
@@ -171,7 +171,7 @@ def create_doc(campaign):
         liste_jours = ["J+0", "J+14", "J+N", "J+21"]
         liste_indice_jours_utiles = []
         for num_jour in range(4):
-            if dico_exposure_condition[i][liste_jours[num_jour]]['date'] != None:
+            if dico_exposure_condition[reference][liste_jours[num_jour]]['date'] != None:
                 liste_indice_jours_utiles.append(num_jour)
         nombre_jours_utiles = len(liste_indice_jours_utiles)
         table_exposure_condition = doc.add_table(
@@ -196,7 +196,7 @@ def create_doc(campaign):
                 paragraph = table_exposure_condition.cell(
                     num_entete+1, num_jour+1).paragraphs[0]
                 paragraph.add_run(str(
-                    dico_exposure_condition[i][liste_jours[liste_indice_jours_utiles[num_jour]]][liste_entete_BDD[num_entete]]))
+                    dico_exposure_condition[reference][liste_jours[liste_indice_jours_utiles[num_jour]]][liste_entete_BDD[num_entete]]))
                 paragraph.alignment = 1
         table_exposure_condition.cell(6, 0).merge(
             table_exposure_condition.cell(6, nombre_jours_utiles))
--- report/word/word_rapport_experimentation_recuperation_donnees.py
+++ report/word/word_rapport_experimentation_recuperation_donnees.py
@@ -11,8 +11,8 @@ def recuperation_donnee(campaign):
     for measurepoint in measurepoints_fusion_id_list:
         data = data_exposure_condition(measurepoint)
         biotest = type_biotest(measurepoint)
-        dico_exposure_condition[int(data[0])] = data[1]
-        dico_type_biotest[int(data[0])] = biotest
+        dico_exposure_condition[data[0]] = data[1]
+        dico_type_biotest[data[0]] = biotest
     dico_avg_tempe, dico_geo_mp = average_temperature__geographic_data_measurepoint(
         measurepoints_fusion_id_list)
     dico_geo_agency = geographic_data_agency(campaign)
@@ -23,7 +23,7 @@ def recuperation_donnee(campaign):
 
 def data_exposure_condition(measurepoint_fusion_id):
     query = QueryScript(
-        f"SELECT DISTINCT substring(reference, -5,2), measurepoint_id FROM key_dates JOIN measurepoint ON measurepoint.id = key_dates.measurepoint_fusion_id WHERE measurepoint_fusion_id = {measurepoint_fusion_id}").execute()
+        f"SELECT DISTINCT reference, measurepoint_id FROM key_dates JOIN measurepoint ON measurepoint.id = key_dates.measurepoint_fusion_id WHERE measurepoint_fusion_id = {measurepoint_fusion_id}").execute()
     measurepoints = [elt[1] for elt in query]
     if len(measurepoints) < 2:
         return query[0][0], data_exposure_condition_simple(measurepoint_fusion_id)
@@ -106,14 +106,14 @@ def average_temperature__geographic_data_measurepoint(measurepoint_fusion_id_lis
     dico_temperature = {}
     dico_geo_data = {}
     tempe = QueryScript(
-        f"SELECT substring(reference, -5,2), sensor3_average, sensor3_min, sensor3_max, latitudeSpotted, longitudeSpotted, lambertXSpotted, lambertYSpotted FROM average_temperature JOIN measurepoint ON average_temperature.measurepoint_fusion_id = measurepoint.id WHERE average_temperature.measurepoint_fusion_id IN {measurepoint_fusion_id_list}").execute()
+        f"SELECT reference, sensor3_average, sensor3_min, sensor3_max, latitudeSpotted, longitudeSpotted, lambertXSpotted, lambertYSpotted FROM average_temperature JOIN measurepoint ON average_temperature.measurepoint_fusion_id = measurepoint.id WHERE average_temperature.measurepoint_fusion_id IN {measurepoint_fusion_id_list}").execute()
     for elt in tempe:
         dico_temp_temperature = {
             'min': elt[2], 'average': elt[1], 'max': elt[3]}
         dico_temp_geo = {'latitudeSpotted': elt[4],
                          'longitudeSpotted': elt[5], 'lambertXSpotted': elt[6], 'lambertYSpotted': elt[7]}
-        dico_temperature[int(elt[0])] = dico_temp_temperature
-        dico_geo_data[int(elt[0])] = dico_temp_geo
+        dico_temperature[elt[0]] = dico_temp_temperature
+        dico_geo_data[elt[0]] = dico_temp_geo
     return dico_temperature, dico_geo_data
 
 # %% Récupération des données géographiques de l'onglet agency
@@ -122,14 +122,12 @@ def average_temperature__geographic_data_measurepoint(measurepoint_fusion_id_lis
 # on entre le nom de la campagne, cela nous ressort les informations géographiques
 def geographic_data_agency(campaign):
     query = QueryScript(
-        f"SELECT substring(place.reference, -2,2), agency.code, agency.name, agency.zipcode, agency.city, agency.stream, agency.lambertX, agency.lambertY, agency.network, agency.hydroecoregion FROM agency JOIN place ON agency.id = place.agency_id JOIN campaign ON place.campaign_id = campaign.id WHERE campaign.reference = '{campaign}';").execute()
-    # query = QueryScript(
-    #     f"SELECT DISTINCT measurepoint.reference, agency.code, agency.name, agency.zipcode, agency.city, agency.stream, agency.lambertX, agency.lambertY, agency.network, agency.hydroecoregion FROM agency JOIN place ON agency.id=place.agency_id JOIN campaign ON place.campaign_id=campaign.id JOIN measurepoint ON measurepoint.place_id=place.id JOIN key_dates ON measurepoint.id=key_dates.measurepoint_fusion_id WHERE campaign.reference='{campaign}'")
+        f"SELECT DISTINCT measurepoint.reference, agency.code, agency.name, agency.zipcode, agency.city, agency.stream, agency.lambertX, agency.lambertY, agency.network, agency.hydroecoregion FROM agency JOIN place ON agency.id=place.agency_id JOIN campaign ON place.campaign_id=campaign.id JOIN measurepoint ON measurepoint.place_id=place.id JOIN key_dates ON measurepoint.id=key_dates.measurepoint_fusion_id WHERE campaign.reference='{campaign}'").execute()
     dico = {}
     for elt in query:
         dico_temp = {'code': elt[1], 'name': elt[2], 'zipcode': elt[3], 'city': elt[4], 'stream': elt[5],
                      'lambertX': elt[6], 'lambertY': elt[7], 'network': elt[8], 'hydroecoregion': elt[9]}
-        dico[int(elt[0])] = dico_temp
+        dico[elt[0]] = dico_temp
     return dico
 
 # %% Récupération type de biotest
