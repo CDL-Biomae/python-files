bfa5e4bce320c9791f888e50e3e3b0723488e132
Thomas Furgé <th.furge@gmail.com>
2020-02-13 16:11:52 +0100

    chemistry to nqe

--- report/style/__init__.py
+++ report/style/__init__.py
@@ -1,5 +1,5 @@
 from .style_survival import *
-from .style_chemistry import *
+from .style_nqe import *
 from .style_physicochimie import *
 from .style_campagnes import *
 from .style_stations import *
