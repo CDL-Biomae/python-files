fd924455808206c86b1e8f16902a0b69e4e3e59d
Tazzz <thomas.pennamen@gmail.com>
2020-02-19 15:40:53 +0100

    Fixed bug

--- report/xl/xl_bbac.py
+++ report/xl/xl_bbac.py
@@ -8,8 +8,7 @@ def create_dataframe(list_mp):
     elements_organic = QueryScript("SELECT sandre, parameter FROM r3 WHERE 21j_threshold IS NOT NULL AND familly!='Métaux'").execute()
     matrix = []
 
-    for i in range(len(list_mp)):
-        mp = list_mp[i]
+    for mp in list_mp:
         pack = QueryScript(f"SELECT id FROM pack WHERE nature='chemistry' AND measurepoint_id={mp}").execute()
         if len(pack)>0:
             metal = chemistry.result_by_pack_and_sandre(pack[0],[int(float(element[0])) for element in elements_metal])
@@ -30,17 +29,12 @@ def create_empty_dataframe(list_mp):
     matrix = []
 
 
-    mp = list_mp[0]
-    pack = QueryScript(f"SELECT id FROM pack WHERE nature='chemistry' AND measurepoint_id={mp}").execute()
-    if len(pack)>0:
+    for mp in list_mp:
         metal_list = ['' for element in elements_metal]
         metal = [metal_list, metal_list]
         organic_list = ['' for element in elements_organic]
         organic = [organic_list, organic_list]
-    else:
-        metal =  [['ND' for el in elements_metal],[int(float(element[0])) for element in elements_metal]]
-        organic =  [['ND' for el in elements_organic],[int(float(element[0])) for element in elements_organic]]
-    matrix.append([''] + metal[0] + [''] + organic[0])
+        matrix.append([''] + metal[0] + [''] + organic[0])
     df = pd.DataFrame(matrix)
     df.columns = [''] + [element[1] for element in elements_metal]  + [''] + [element[1] for element in elements_organic]
     df = df.dropna(how='all', axis='columns')
@@ -69,6 +63,7 @@ def create_bbac2_dataframe(head_dataframe, list_campaigns, dict_mp):
         list_dataframe.append(df)
 
     df_values = pd.concat(list_dataframe)
+    print(head_dataframe.shape, df_values.shape)
     df_concat = pd.concat([head_dataframe, df_values], axis=1)
     df_campaigns = df_concat.sort_values(['Numéro', 'Campagne'])
 
