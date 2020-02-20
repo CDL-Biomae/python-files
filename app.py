73187050c1a817cc1cbe5071bf0d6c7d50e32711
Tazzz <thomas.pennamen@gmail.com>
2020-02-20 14:58:24 +0100

    .

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import tkinter as tk
+from tkinter import filedialog
 from report import main
 
 def main_button(campaign_input):
-    print(campaign_input.get())
+    print(campaign_input.get)
 
 def reset():
     campaign_input.set()
