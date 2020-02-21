e6865a4c81c977380ca0ecf9fe1b4ce8945f3a4b
Julien Bronner <julien.bronner@ecl17.ec-lyon.fr>
2020-02-21 16:43:26 +0100

    Word experimentation ajout water

--- report/word/word_rapport_experimentation_creation_fichier.py
+++ report/word/word_rapport_experimentation_creation_fichier.py
@@ -100,6 +100,7 @@ def word_main(campaign, agence, path_photo="Photos", path_output="output"):
 
         table_carte = doc.add_table(rows=4, cols=1)
         access_token = "pk.eyJ1IjoiamJyb25uZXIiLCJhIjoiY2s2cW5kOWQwMHBybjNtcW8yMXJuYmo3aiJ9.z8Ekf7a0RGTZ4jrbJVpq8g"
+        layer = '{"id":"water","source":{"url":"mapbox://mapbox.mapbox-streets-v8","type":"vector"},"source-layer":"water","type":"fill","paint":{"fill-color":"%2300ffff"}}'
         url_street = f"https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+FF0000({str(dico_geo_mp[reference]['longitudeSpotted'])},{str(dico_geo_mp[reference]['latitudeSpotted'])})/{str(dico_geo_mp[reference]['longitudeSpotted'])},{str(dico_geo_mp[reference]['latitudeSpotted'])},9.21/450x300@2x?access_token={access_token}"
         response = requests.get(url_street)
         carte_street = BytesIO(response.content)
@@ -109,7 +110,8 @@ def word_main(campaign, agence, path_photo="Photos", path_output="output"):
         table_carte.cell(1, 0).text = "Vue carte situant les villes alentours"
         table_carte.cell(1, 0).paragraphs[0].alignment = 1
 
-        url_satellite = f"https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/pin-s+FF0000({str(dico_geo_mp[reference]['longitudeSpotted'])},{str(dico_geo_mp[reference]['latitudeSpotted'])})/{str(dico_geo_mp[reference]['longitudeSpotted'])},{str(dico_geo_mp[reference]['latitudeSpotted'])},13.5/450x300@2x?access_token={access_token}"
+        url_satellite = f"https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/pin-s+FF0000({str(dico_geo_mp[reference]['longitudeSpotted'])},{str(dico_geo_mp[reference]['latitudeSpotted'])})/{str(dico_geo_mp[reference]['longitudeSpotted'])},{str(dico_geo_mp[reference]['latitudeSpotted'])},13.5/450x300@2x?addlayer={layer}&access_token={access_token}"
+        print(url_satellite)
         response = requests.get(url_satellite)
         carte_satellite = BytesIO(response.content)
         table_carte.cell(2, 0).paragraphs[0].add_run().add_picture(
