328eba2a64deb5dfb6760b34f47d417946a93000
Tazzz <thomas.pennamen@gmail.com>
2020-02-25 15:54:51 +0100

    Deleted comments

--- calcul/toxicity/neurotoxicity.py
+++ calcul/toxicity/neurotoxicity.py
@@ -4,23 +4,6 @@ def neurotoxicity(dict_pack_fusion):
     
     constant_AChE = QueryScript(
         "SELECT value FROM r2_constant WHERE name LIKE 'Constante ache%'").execute()
-    # SQL_request = "SELECT ache, weight FROM cage WHERE pack_id="+str(pack_id)
-    # output = QueryScript(SQL_request).execute()
-
-    # AChE_list = []
-    # weight_list = []
-
-    # for element in output :
-    #     if element[0]!=None:
-    #         AChE_list.append(element[0])
-    #         weight_list.append(element[1])
-            
-    # mean_AChE = sum(AChE_list)/len(AChE_list)
-    # mean_weight = sum(weight_list)/len(weight_list)
-
-    # AChE_expected = constant_AChE[0] + constant_AChE[1] * ( mean_weight ** constant_AChE[2] )
-    
-    # return ( mean_AChE - AChE_expected ) / AChE_expected * 100
 
     pack_dict = {}
     for element in dict_pack_fusion:
