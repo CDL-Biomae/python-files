6e1064bfb99377e8cfd7ebfdc12f7ca45692e6c4
Thomas Furgé <th.furge@gmail.com>
2020-02-25 11:20:02 +0100

    Updated female_survivor and number_female_concerned_area to dict version

--- calcul/toxicity/reproduction.py
+++ calcul/toxicity/reproduction.py
@@ -1,20 +1,52 @@
 from tools import QueryScript
-from math import *
 
 
-def female_survivor(pack_id):
-    SQL_request = "SELECT scud_survivor_female,scud_quantity FROM cage where pack_id="+str(pack_id)+" and scud_survivor_female is not null "
-    f_survivor = []
-    quantity =[]
-    resultat =  QueryScript(SQL_request).execute()
-    for element in resultat :
-            f_survivor.append(element[0])
-            quantity.append(element[1])
+def female_survivor(dict_pack_fusion):
+    nature = 'reproduction'
+    list_pack_repro = []
+    list_mp_repro = []
+    for mp in dict_pack_fusion:
+        try:
+            pack_id = dict_pack_fusion[mp][nature]
+        except KeyError:
+            pass
+        else:
+            list_mp_repro.append(mp)
+            list_pack_repro.append(pack_id)
 
-    if sum(f_survivor) == None:
-        return "NA"
-    else:
-        return round(sum(f_survivor)/len(f_survivor)/quantity[0]*100)
+    output = QueryScript(
+        f"SELECT pack_id, scud_survivor_female, scud_quantity FROM cage WHERE pack_id IN {tuple(list_pack_repro)};"
+    ).execute()
+
+    # Reformatage des données de la requête
+    dict_requete = {pack_id: {'scud_quantity': None, 'scud_survivor_female': []} for pack_id in list_pack_repro}
+
+    for row in output:
+        [pack_id, scud_survivor_female, scud_quantity] = row
+        if dict_requete[pack_id]['scud_quantity'] is None:
+            dict_requete[pack_id]['scud_quantity'] = scud_quantity
+        if scud_survivor_female is not None:
+            dict_requete[pack_id]['scud_survivor_female'].append(scud_survivor_female)
+
+    # Calcul du pourcentage de survie par pack
+    dict_survie_femelle = {mp_fusion: 'ND' for mp_fusion in dict_pack_fusion.keys()}
+
+    for mp_fusion in dict_survie_femelle.keys():
+        if mp_fusion not in list_mp_repro:
+            continue
+        pack_id = dict_pack_fusion[mp_fusion]['reproduction']
+        scud_quantity = dict_requete[pack_id]['scud_quantity']
+        scud_survivor_female = dict_requete[pack_id]['scud_survivor_female']
+
+        if scud_quantity is not None and len(scud_survivor_female) != 0:
+            try:
+                survie_femelle = (sum(scud_survivor_female)/len(scud_survivor_female)) / scud_quantity
+            except ZeroDivisionError:
+                print(f"Point de mesure: {mp_fusion} --> Erreur de calcul de survie femelle, quantité de gamares dans la cage inconnu")
+                continue
+            dict_survie_femelle[mp_fusion] = f"{survie_femelle*100}"
+
+    return dict_survie_femelle
 
     
 
