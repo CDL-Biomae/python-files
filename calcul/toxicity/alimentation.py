5d4f5e60e39b7759c09197b76e519b93edc92d59
Thomas Furgé <th.furge@gmail.com>
2020-02-26 15:57:01 +0100

    changement des dernières fonctions de calcul en dict_version

--- calcul/toxicity/alimentation.py
+++ calcul/toxicity/alimentation.py
@@ -14,18 +14,19 @@ def survie_alim(dict_pack_fusion):
     pack_checked = None
     current_quantity = None
     for cage in survivor_list:
-        if pack_checked!=cage[0]:
+        if pack_checked != cage[0]:
             if pack_checked:
                 result[pack_dict[pack_checked]]['average'] = sum([(result[pack_dict[pack_checked]]['replicate'][replicate]*2-current_quantity)/current_quantity for replicate in result[pack_dict[pack_checked]]['replicate']])
             pack_checked = cage[0]
             current_quantity = None
             if cage[2]:
                 current_quantity = cage[2]
-                result[pack_dict[pack_checked]] = {'replicate': {cage[3] : (cage[1]+current_quantity)/2}} 
-        else :
+                result[pack_dict[pack_checked]] = {'replicate': {cage[3]: (cage[1]+current_quantity)/2}}
+        else:
             result[pack_dict[pack_checked]]['replicate'][cage[3]] = (cage[1]+current_quantity)/2 
 
     return result
+
 
 def alimentation(dict_pack_fusion):
     pack_dict = {}
@@ -33,8 +34,8 @@ def alimentation(dict_pack_fusion):
         try:
             pack_dict[dict_pack_fusion[element]['alimentation']] = element 
         except KeyError:
-            None 
-    result = {element:None for element in dict_pack_fusion}
+            pass
+    result = {element: None for element in dict_pack_fusion}
     
     ################### Calcul des tailles des spécimens
     specimen_size_data =  QueryScript(f"  SELECT pack_id, individual, size_px, size_mm   FROM {env.DATABASE_RAW}.measuresize WHERE pack_id IN {tuple([element for element in pack_dict])}").execute()
@@ -44,38 +45,38 @@ def alimentation(dict_pack_fusion):
     current_specimen_sample = []
     is_in_mm = False
     for size in specimen_size_data:
-        if pack_checked!=size[0]:
+        if pack_checked != size[0]:
             if pack_checked and ratio:
                 specimen_size[pack_dict[pack_checked]] = [element*ratio for element in current_specimen_sample]
             elif pack_checked and is_in_mm:
                 specimen_size[pack_dict[pack_checked]] = [element for element in current_specimen_sample]
             pack_checked = size[0]
             ratio = None
-            is_in_mm=False
+            is_in_mm = False
             current_specimen_sample = []
-            if size[1]=='0' and size[2]:
+            if size[1] == '0' and size[2]:
                 ratio = size[3]/size[2]
-            else :
-                if size[3] and size[3]!=0:
+            else:
+                if size[3] and size[3] != 0:
                     is_in_mm = True
                     current_specimen_sample = [size[3]]
-                else :
+                else:
                     current_specimen_sample = [size[2]]
                     
-        else :
+        else:
             
-            if size[1]=='0' and size[2]:
+            if size[1] == '0' and size[2]:
                 ratio = size[3]/size[2]
-            else :
-                if size[3] and size[3]!=0 and not ratio:
+            else:
+                if size[3] and size[3] != 0 and not ratio:
                     is_in_mm = True
                     current_specimen_sample.append(size[3])
-                else :
+                else:
                     current_specimen_sample.append(size[2])     
     ############################################
     
     ############### Calcul des tailles feuilles ingérées
-     
+
     standard_leaf_number = QueryScript(
         f" SELECT value   FROM {env.DATABASE_TREATED}.r2_constant WHERE name='Nombre de disques (témoin)' WHERE version={env.VERSION}").execute()[0]
     replicate_leaf_number = QueryScript(
@@ -86,22 +87,19 @@ def alimentation(dict_pack_fusion):
     remaining_leaves = {element:None for element in dict_pack_fusion}
     pack_checked = None
     for leaf in remaining_leaves_data:
-        pack_checked=leaf[0]
+        pack_checked = leaf[0]
         if remaining_leaves[pack_dict[pack_checked]]:
             if leaf[1] in remaining_leaves[pack_dict[pack_checked]]:
-                remaining_leaves[pack_dict[pack_checked]][leaf[1]]+= leaf[2]
-            else :
-                remaining_leaves[pack_dict[pack_checked]][leaf[1]]= leaf[2]
+                remaining_leaves[pack_dict[pack_checked]][leaf[1]] += leaf[2]
+            else:
+                remaining_leaves[pack_dict[pack_checked]][leaf[1]] = leaf[2]
         
-        else :
+        else:
             remaining_leaves[pack_dict[pack_checked]]= {leaf[1]:leaf[2]}
                 
     ##### Conversion pixel restants -> mm2 consommées par individu par jour
-            
-       
-            
     survivor = survie_alim(dict_pack_fusion)      
-    eaten_leaves = {element:None for element in dict_pack_fusion}
+    eaten_leaves = {element: None for element in dict_pack_fusion}
     for element in remaining_leaves:
         if remaining_leaves[element] and survivor[element]['replicate']:   
             replicate_raw_value = remaining_leaves[element][0]/standard_leaf_number*replicate_leaf_number if 0 in remaining_leaves[element] else None
@@ -119,7 +117,7 @@ def alimentation(dict_pack_fusion):
     average_temperature_output = QueryScript(f" SELECT measurepoint_fusion_id, sensor1_average   FROM {env.DATABASE_TREATED}.average_temperature WHERE measurepoint_fusion_id IN {tuple(dict_pack_fusion)} AND version={env.VERSION}").execute()
     for element in average_temperature_output:
         average_temperature[element[0]] = element[1]
-    for element in dict_pack_fusion :
+    for element in dict_pack_fusion:
         mean_size = None
         if specimen_size[element]:
             
@@ -136,5 +134,3 @@ def alimentation(dict_pack_fusion):
                         inhibition[element] = sum(sorted_inhibition_list)/len(sorted_inhibition_list)*100   
 
     return inhibition
-    
-   
