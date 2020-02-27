ac05a01c6afeedc70247ab8b6b9d183ebf5c4641
Tazzz <thomas.pennamen@gmail.com>
2020-02-27 13:41:03 +0100

    .

--- database/fill.py
+++ database/fill.py
@@ -1,9 +1,8 @@
 from . import *
 
-def run(cas=None):
-    reference_filler.run()
-    reference_date_filler.run()
-    date_filler.run()
-    average_temperature_filler.run()
-    if cas :
-        tox_table_filler.run(cas)
+def run(cas):
+    reference_filler.run(cas)
+    date_filler.run(cas)
+    average_temperature_filler.run(cas)
+    temperature_repro.run(cas)
+    tox_table_filler.run(cas)
