a1b3657a8834656e0c08b3b249a7b39a8e4ba215
Thomas Furgé <th.furge@gmail.com>
2020-03-03 14:18:19 +0100

    Fin de l'ajout des cas 1,2,3 pour l'ajout de version

--- database/fill.py
+++ database/fill.py
@@ -3,12 +3,12 @@ from . import *
 
 def run(cas):
     version_filler.run(cas)
-    reference_filler.run()
+    reference_filler.run(cas)
 
     reference_date_filler.run(cas)
     date_filler.run(cas)
 
-    average_temperature_filler.run()
-    temperature_repro.run()
+    average_temperature_filler.run(cas)
+    temperature_repro.run(cas)
 
     tox_table_filler.run(cas)
