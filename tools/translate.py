943e1b6410f5a043ef1f5b75c1a69ed2f992906e
Thomas Furgé <th.furge@gmail.com>
2020-03-04 15:55:03 +0100

    Debug de translate

--- tools/translate.py
+++ tools/translate.py
@@ -1,6 +1,6 @@
 def translate(element):
     if isinstance(element, str):
-        element.replace("‰", "\\x89")
+        element = element.replace("‰", "\x89")
         return element.encode('iso-8859-1').decode('utf-8')
     else:
         return element
