1f494dee5fc13a7adc0cb50be7d041fbecf7f478
Tazzz <thomas.pennamen@gmail.com>
2020-03-04 10:34:58 +0100

    Added translate

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,3 +2,4 @@ from .query import QueryScript
 from .list_to_dict import list_to_dict
 from .agency_finder import *
 from .clean_dict import *
+from .translate import translate
---
+++ tools/translate.py
@@ -0,0 +1,2 @@
+def translate(string):
+    return string.encode('iso-8859-1').decode('utf-8')
