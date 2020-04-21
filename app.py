007ffeeb9cb984df030082e2dc18304a270bc421
unknown <thomas.pennamen@gmail.com>
2020-04-21 20:02:05 +0200

    Rename for pyinstaller compatibility

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from app import MainApp
+from log import MainApp
 
-app = MainApp()
-app.title("Digital Lab App")
-app.geometry("540x360+100+100")
-app.minsize(480, 360)
-if not app.has_to_quit:
-    app.mainloop()
+if __name__ =="__main__":
+    app = MainApp()
+    app.title("Digital Lab App")
+    app.geometry("540x360+100+100")
+    app.minsize(480, 360)
+    if not app.has_to_quit:
+        app.mainloop()
