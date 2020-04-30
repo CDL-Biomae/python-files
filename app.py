525b32e0a609136eb094c88b266a362145b81ca1
unknown <thomas.pennamen@gmail.com>
2020-04-30 18:57:56 +0200

    Changed size

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ if __name__ =="__main__":
     app = MainApp()
     app.title("Digital Lab App")
     app.geometry("540x360+100+100")
-    app.minsize(480, 360)
+    app.minsize(540, 360)
     if not app.has_to_quit:
         app.mainloop()
